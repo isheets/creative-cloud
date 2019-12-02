cca723ff273c7238a718c17ed5cbe650bf56dfc9
Isaac Sheeets <icsheets97@gmail.com>
2019-12-02 10:54:50 -0700

    added basic lightbox functionality

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,5 +1,44 @@
 console.log("js working");
 
-var tabs = new Tabby("[data-tabs]");
+window.addEventListener('DOMContentLoaded', (event) => {
+    let projectThumbs = document.getElementsByClassName('project-thumb');
+
+    for(let thumb of projectThumbs) {
+        thumb.addEventListener('click', openProjectContent);
+    }
+});
+
+//opens lightbox with content for requested project
+var openProjectContent = (e) => {
+
+    let projectClicked = e.srcElement.id;
+    let contentToRender = null;
+
+    switch(projectClicked) {
+        case "twitter-emojis":
+            contentToRender = twitterEmojiContent;
+            break;
+        case "syllabus-archive":
+            contentToRender = syllabusArchiveContent;
+            break;
+        case "habits":
+            contentToRender = habitsContent;
+            break;
+        case "driving-sucks":
+            contentToRender = drivingSucksContent;
+        case "finals-countdown":
+            contentToRender = finalsCountdownContent;
+        default:
+            break;
+    }
+
+    if(contentToRender !== null) {
+        SimpleLightbox.open({
+            content: contentToRender,
+            elementClass: 'slbContentEl'
+        });
+    }
+}
+
 
 
